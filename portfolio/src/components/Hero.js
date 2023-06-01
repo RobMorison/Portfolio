3718e323f1d022e9c599a484ebd376b3c408c66b
Sara Morison <rob.morison32@gmail.com>
2023-06-01 14:30:16 -0400

    Created Hero.js + Hero.css

---
+++ portfolio/src/components/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Hero.css";
+
+
+const Hero = () => {
+    return ( 
+        <>
+        <div className='hero'>
+            <p>Amet nostrud est officia culpa ex. Non et non incididunt nostrud culpa amet voluptate nulla adipisicing sit ad laboris. Mollit ipsum ex reprehenderit dolor quis proident aliquip reprehenderit sint irure labore.</p>
+            <p>Incididunt minim non aliquip sit nisi fugiat excepteur nostrud eiusmod qui nisi reprehenderit adipisicing. Ut ex do minim eiusmod exercitation ut aute voluptate cupidatat exercitation eiusmod incididunt. Cillum laboris occaecat occaecat duis consequat reprehenderit deserunt anim duis aute.</p>
+            <p>Ipsum dolor cillum elit non pariatur laborum sit.</p>
+                <div className='bio'>
+                    <span className='name'>Rob Morison</span>
+                    <span>Full Stack Developer</span>
+                </div>
+        </div>
+        </>
+     );
+}
+ 
+export default Hero;
