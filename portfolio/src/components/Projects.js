404a8ab375223727b4fafe62572a1ea7d2dbbf18
Sara Morison <rob.morison32@gmail.com>
2023-06-02 15:06:33 -0400

    Created Projects file and added projects objects

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Projects.css';
+
+const projectsData = [
+    {
+        name: 'E-Commerce Website',
+        link: 'https://github.com/RobMorison/Capstone'
+    },
+    {
+        name: 'Music Library Frontend',
+        link: 'https://github.com/RobMorison/music_library_frontend'
+    },
+    {
+        name: 'Music Library Backend',
+        link: 'https://github.com/RobMorison/music_library_backend'
+    },
+    {
+        name: 'Youtube Clone',
+        link: 'https://github.com/RobMorison/Youtube_Clone'
+    },
+]
+
+const Projects = () => {
+    return ( 
+        <h1>Projects</h1>
+     );
+}
+ 
+export default Projects;
