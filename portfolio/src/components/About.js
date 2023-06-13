90907ad1e0148273f1766231baa41e287922ebf3
Sara Morison <rob.morison32@gmail.com>
2023-06-13 10:35:29 -0400

    Added selfie picture + created box around about me section and added picture to about me

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './About.css';
+import selfie from '../assets/selfie.png'
 
 const About = () => {
     return ( 
-        <><h1 className='about'>Hello World</h1>
-        <div>
-            <p className='about-me'>
-                Junior Software Developer with a focus on developing my skills everyday that embraces a fast-paced team
-                environement.  Striving to work with team members and project leaders to solve problems and create functioning
-                and aestetically pleasing application.  With an understanding of the full development cycle and working with front and 
-                back end techologies
-            </p>
+        <><h2>About Me</h2>
+        <div className='about'>
+            <div>
+                <p className='about-me'>
+                    Junior Software Developer with a focus on developing my skills everyday that embraces a fast-paced team
+                    environement.  Striving to work with team members and project leaders to solve problems and create functioning
+                    and aestetically pleasing application.  With an understanding of the full development cycle and working with front and 
+                    back end techologies
+                </p>
+            </div>
+            <img src={selfie} alt='picure of myself'></img>
         </div>
         </>
      );
