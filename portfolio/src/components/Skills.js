91a50b21e068f8c820317f23707ef0e3bbfec9cf
Sara Morison <rob.morison32@gmail.com>
2023-06-13 15:00:00 -0400

    created Skills grid and design + added logo picture to meta head data for website tab

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -6,28 +6,34 @@ const Skills = () => {
         <>
         <h2>Skills</h2>
         <div className='skills'>
-            <p>Languages:</p>
-            <ul>
-                <li>Python</li>
-                <li>JavaScript</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>SQL</li>
-            </ul>
-            <p>Libraries & Frameworks</p>
-            <ul>
-                <li>Django Rest Framework</li>
-                <li>React.js</li>
-                <li>Bootstrap</li>
-                <li>Third-Party APIs</li>
-            </ul>
-            <p>Tools:</p>
-            <ul>
-                <li>Postman</li>
-                <li>MySQL</li>
-                <li>Visual Studio Code</li>
-                <li>GitHub</li>
-            </ul>
+            <div className='languages'>
+                <p>Languages:</p>
+                <div className='grid-container'>
+                        <div class='grid-item'>Python</div>
+                        <div class='grid-item'>JavaScript</div>
+                        <div class='grid-item'>HTML</div>
+                        <div class='grid-item'>CSS</div>
+                        <div class='grid-item'>SQL</div>
+                </div>
+            </div>
+            <div>
+                <p>Libraries & Frameworks</p>
+                <div className='grid-container'>
+                    <div class='grid-item'>Django Rest Framework</div>
+                    <div class='grid-item'>React.js</div>
+                    <div class='grid-item'>Bootstrap</div>
+                    <div class='grid-item'>Third-Party APIs</div>
+                </div>
+            </div>
+            <div>
+                <p>Tools:</p>
+                <div className='grid-container'>
+                    <div class='grid-item'>Postman</div>
+                    <div class='grid-item'>MySQL</div>
+                    <div class='grid-item'>Visual Studio Code</div>
+                    <div class='grid-item'>GitHub</div>
+                </div>
+            </div>
         </div>
         </>
      );
